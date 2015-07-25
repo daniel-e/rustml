6be5cdd5f8c45552c43fd4848a753f7f720faebc
Ano Nym <detzold@gmail.com>
2015-07-25 16:37:03 +0200

    misc

--- examples/video_histogram.rs
+++ examples/video_histogram.rs
@@ -1,23 +1,37 @@
 extern crate rustml;
 
 use std::io::Write;
+use std::fs::File;
+
 use rustml::*;
 use rustml::opencv::*;
+use rustml::consts::*;
 
+// note: to be able to run this example you need the rustml dataset package
+// see https://github.com/daniel-e/rustml#datasets
+//
+// The result of this example can be plotted with Octave as follows:
+// start Octave and type:
+// > load("/tmp/day_and_night.txt");
+// > hist(day_and_night, 256);
 fn main() {
-    // TODO example files
-    let video = Video::from_file("/home/dz/videos/generated/reduced.avi").unwrap();
-    let mask = GrayImage::from_file("/home/dz/videos/generated/mask.png").unwrap();
+    let video = Video::from_file(
+        &consts::path_for("videos/day_and_night_in_gray.avi").unwrap()).unwrap();
+    let mask = GrayImage::from_file(
+        &consts::path_for("videos/day_and_night_in_gray_mask_sky.png").unwrap()).unwrap();
+
+    let mut f = File::create("/tmp/day_and_night.txt").unwrap();
 
     for (idx, img) in video.gray_frame_iter().enumerate() {
 
+        // write status to stderr for the impatient
         if idx % 1000 == 0 {
             writeln!(&mut std::io::stderr(), "{} frames", idx).unwrap();
         }
 
-        println!("{}", 
+        writeln!(f, "{}",
             img.mask_iter(&mask).map(|x| x as u32).collect::<Vec<u32>>().mean()
-        );
+        ).unwrap();
     }
 }
 
