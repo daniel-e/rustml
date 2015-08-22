9b53432e9018ba0147127fd70b8066e06d740d51
Ano Nym <detzold@gmail.com>
2015-08-22 11:39:50 +0200

    refactor

--- examples/gradient_descent.rs
+++ examples/gradient_descent.rs
@@ -1,20 +1,23 @@
 extern crate rustml;
+extern crate num;
 
 use rustml::opt::*;
+use num::pow;
 
 fn main() {
 
+    // set the number of iterations to 10
     let opts = OptParams::new().iter(10);
 
     let r = opt(
-        |x| (x[0] - 2.0) * (x[0] - 2.0), 
-        |x| vec![2.0 * (x[0] - 2.0)], 
-        &[4.0], 
-        opts
+        |p| pow(p[0] - 2.0, 2),       // objective to be minimized: (x-2)^2
+        |p| vec![2.0 * (p[0] - 2.0)], // derivitive
+        &[4.0],                       // initial parameters
+        opts                          // optimization options
     );
 
     for (iter, i) in r.fvals.iter().enumerate() {
         println!("error after iteration {} was {}", iter + 1, i);
     }
-    println!("{:?}", r.params);
+    println!("solution: {:?}", r.params);
 }
--- src/opt.rs
+++ src/opt.rs
@@ -21,8 +21,8 @@ impl <T: Clone> OptParams<T> {
     /// 
     /// ```
     /// use rustml::opt::*;
-    /// let opts = OptParams::new();
-    /// assert!(opts.alphs.is_none());
+    /// let opts = default_opts();
+    /// assert!(opts.alpha.is_none());
     /// ```
     pub fn new() -> OptParams<T> {
         OptParams {
@@ -38,7 +38,7 @@ impl <T: Clone> OptParams<T> {
     ///
     /// ```
     /// use rustml::opt::*;
-    /// let opts = OptParams::new().alpha(0.2);
+    /// let opts = default_opts().alpha(0.2);
     /// assert_eq!(opts.alpha.unwrap(), 0.2);
     /// ```
     pub fn alpha(&self, val: T) -> OptParams<T> {
@@ -55,7 +55,7 @@ impl <T: Clone> OptParams<T> {
     ///
     /// ```
     /// use rustml::opt::*;
-    /// let opts = OptParams::new().iter(100);
+    /// let opts = default_opts().iter(100);
     /// assert_eq!(opts.iter.unwrap(), 100);
     /// ```
     pub fn iter(&self, val: usize) -> OptParams<T> {
@@ -72,7 +72,7 @@ impl <T: Clone> OptParams<T> {
     ///
     /// ```
     /// use rustml::opt::*;
-    /// let opts = OptParams::new().eps(0.01);
+    /// let opts = default_opts().eps(0.01);
     /// assert_eq!(opts.eps.unwrap(), 0.01);
     /// ```
     pub fn eps(&self, val: T) -> OptParams<T> {
@@ -84,8 +84,13 @@ impl <T: Clone> OptParams<T> {
     }
 }
 
+pub fn default_opts() -> OptParams<f64> {
+    OptParams::new()
+}
+
+/// The result of an optimization.
 pub struct OptResult<T> {
-    /// The values of the objective function after each iteration.
+    /// The value of the objective function after each iteration.
     pub fvals: Vec<T>,
     /// The parameters after the last iteration.
     pub params: Vec<T>,
@@ -96,7 +101,7 @@ pub struct OptResult<T> {
 /// Minimizes an objective using gradient descent.
 ///
 /// The objective `f` is minimized using a standard gradient descent algorithm. The
-/// argument `fd` must return the values of the derivitives for each parameter
+/// argument `fd` must return the values of the derivatives for each parameter
 /// and is executed in each iteration for the current parameters. The argument
 /// `init` contains the initial parameters and `opts` contains the options
 /// for the gradient descent algorithm.
@@ -112,8 +117,9 @@ pub struct OptResult<T> {
 /// the parameters $\theta_0, \dots, \theta_n$ the algorithm works as follows:
 ///
 /// <div style="padding-left: 10px; background: #eeeeee">
-/// $[\theta_0, \dots, \theta_n] \leftarrow init$ <br>
+/// $[\theta_0, \dots, \theta_n] \leftarrow$ init <br>
 /// $\alpha \leftarrow$ opts.alpha <br>
+/// $\epsilon \leftarrow$ opts.epsilon <br>
 /// <br>
 /// for i = 1 to opts.iter do <br>
 /// <div style="padding-left:20px"> 
@@ -138,6 +144,33 @@ pub struct OptResult<T> {
 /// If `alpha` is not specified in `opts` the value 0.1 is used. If the number of
 /// iterations is not specified in `opts` the value 1000 is used. If `epsilon` is
 /// not specified in `opts` no stopping criterion is checked.
+///
+/// # Example
+/// 
+/// ```
+/// # extern crate rustml;
+/// # extern crate num;
+/// use rustml::opt::*;
+/// use num::pow;
+///
+/// # fn main() {
+/// // set the number of iterations to 10
+/// let opts = default_opts().iter(10);
+///
+/// let r = opt(
+///     |p| pow(p[0] - 2.0, 2),       // objective to be minimized: (x-2)^2
+///     |p| vec![2.0 * (p[0] - 2.0)], // derivative
+///     &[4.0],                       // initial parameters
+///     opts                          // optimization options
+/// );
+///
+/// for (iter, i) in r.fvals.iter().enumerate() {
+///     println!("error after iteration {} was {}", iter + 1, i);
+/// }
+/// println!("solution: {:?}", r.params);
+/// assert!(r.params[0] - 2.0 <= 0.3);
+/// # }
+/// ```
 pub fn opt<O, D>(f: O, fd: D, init: &[f64], opts: OptParams<f64>) -> OptResult<f64>
     where O: Fn(&[f64]) -> f64, D: Fn(&[f64]) -> Vec<f64> {
 
