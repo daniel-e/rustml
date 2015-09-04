984d1685205c5fbf9c53eaa028c940c78a6becab
Your Name <detzold@gmail.com>
2015-09-04 21:38:04 +0200

    doc

--- src/blas.rs
+++ src/blas.rs
@@ -1,5 +1,41 @@
 //! Bindings for BLAS/ATLAS for high performance vector and matrix operations.
-
+//!
+//! from [here](http://www.netlib.org/blas/):
+//!
+//! <i>The BLAS (Basic Linear Algebra Subprograms) are routines that provide standard building blocks
+//! for performing basic vector and matrix operations. The Level 1 BLAS perform scalar, vector and
+//! vector-vector operations, the Level 2 BLAS perform matrix-vector operations, and the Level 3
+//! BLAS perform matrix-matrix operations. Because the BLAS are efficient, portable, and widely
+//! available, they are commonly used in the development of high quality linear algebra
+//! software.</i>
+//!
+//! There are several implementations of BLAS:
+//!
+//! * [OpenBLAS](http://www.openblas.net/)
+//! * [ATLAS (automatically tuned linear algebra software)](http://math-atlas.sourceforge.net/)
+//! * [Netlib BLAS](http://www.netlib.org/blas/)
+//! * [Intel MKL](https://software.intel.com/en-us/intel-mkl)
+//! * and some more
+//!
+//! The Netlib BLAS implementation (reference implementation) is usually required when
+//! compiling rustml but you can switch to
+//! any of these implementations without recompiling rustml simply by setting the
+//! `LD_PRELOAD` environment variable to the location of the library you want to use
+//! before running your application.
+//!
+//! Example: Let's assume you have installed ATLAS into `/opt/atlas` and want to start you
+//! application with cargo. Then, you can use the
+//! ATLAS implementation simply by starting your application as follows (depending on your
+//! installation):
+//!
+//! ```ignore
+//! LD_PRELOAD=/opt/atlas/lib/libtatlas.so cargo run myapp
+//! ```
+//!
+//! # Using BLAS for vector and matrix operations
+//!
+//! This module provides low level functions. ...
+//! 
 extern crate libc;
 
 use self::libc::{c_int, c_double, c_float};
@@ -34,14 +70,16 @@ pub enum Transpose {
 extern {
     // TODO wrapper functions
 
-    /// Computes the L2 norm of a vector of f64 (doubles).
-    pub fn cblas_dnrm2(n: c_int, x: *const c_double, incx: c_int) -> c_double;
-
-    /// Computes the L2 norm of a vector of f32 (floats).
-    pub fn cblas_snrm2(n: c_int, x: *const c_float, incx: c_int) -> c_float;
-
-    /// Computes `alpha * x + y` where `alpha` is a f64 scalar and `x` and `y` are 
-    /// vectors of f64.
+    /// Computes `alpha * x + y` and stores the result in `y`.
+    ///
+    /// The paramters `alpha` is a f64 scalar and `x` and `y` are 
+    /// vectors with elements of type f64. The parameter `n` specifies
+    /// the number of elements in `x` and `y`. The parameters `incx`
+    /// and `incy` specify the increments between the elements in 
+    /// vector `x` and `y` respectively.
+    ///
+    /// For a high level abstraction you should use [d_axpy](../ops_inplace/fn.d_axpy.html)
+    /// in the module [ops_inplace](../ops_inplace/index.html).
     pub fn cblas_daxpy(
         n: c_int, 
         alpha: c_double, 
@@ -51,21 +89,13 @@ extern {
         incy: c_int
     );
 
-    /// Computes `alpha * x + y` where `alpha` is a f64 scalar and `x` and `y` are 
-    /// vectors of f32.
-    pub fn cblas_saxpy(
-        n: c_int, 
-        alpha: c_float, 
-        x: *const c_float, 
-        incx: c_int, 
-        y: *mut c_float, 
-        incy: c_int
-    );
-
-    /// Computes `alpha * op(A) * op(B) + beta * C` where `alpha` and
+    /// Computes `alpha * op(A) * op(B) + beta * C` and stores the result in `C` where `alpha` and
     /// `beta` are f64 scalars, `A`, `B`, `C` are a matrices of f64 values and `op(X)` is either
     /// `op(X) = X` or `op(X) = X^T` (the transpose or conjugate transpose of
     /// the matrix `X`).
+    ///
+    /// For a high level abstraction you should use [d_gemm](../ops_inplace/fn.d_gemm.html)
+    /// in the module [ops_inplace](../ops_inplace/index.html).
     pub fn cblas_dgemm(
         order: Order, transA: Transpose, transB: Transpose,
         m: c_int,
@@ -76,6 +106,24 @@ extern {
         B: *const c_double, ldb: c_int,
         beta: c_double,
         C: *mut c_double, ldc: c_int
+    );
+
+
+    /// Computes the L2 norm of a vector of f64 (doubles).
+    pub fn cblas_dnrm2(n: c_int, x: *const c_double, incx: c_int) -> c_double;
+
+    /// Computes the L2 norm of a vector of f32 (floats).
+    pub fn cblas_snrm2(n: c_int, x: *const c_float, incx: c_int) -> c_float;
+
+    /// Computes `alpha * x + y` where `alpha` is a f64 scalar and `x` and `y` are 
+    /// vectors of f32.
+    pub fn cblas_saxpy(
+        n: c_int, 
+        alpha: c_float, 
+        x: *const c_float, 
+        incx: c_int, 
+        y: *mut c_float, 
+        incy: c_int
     );
 
     /// Computes `alpha * op(A) * op(B) + beta * C` where `alpha` and
