1eb7c2926244f8f737e2f75ee12b9c4c7c1e1e65
Ano Nym <detzold@gmail.com>
2015-10-04 19:57:21 +0200

    refactor

--- examples/neuralnetwork.rs
+++ examples/neuralnetwork.rs
@@ -11,7 +11,6 @@ use rustml::*;
 // the XOR function.
 
 fn main() {
-
     // create the dataset
     let seed = [1, 2, 3, 4];
     let n = 50;
@@ -24,19 +23,18 @@ fn main() {
             .rm_column(0).unwrap();
 
     // create the labels
-    let labels = repeat(0.0).take(2 * n).chain(repeat(1.0).take(2 * n))
-        .collect::<Vec<_>>();
+    let labels = Matrix::from_iter(
+            repeat(0.0).take(2 * n).chain(repeat(1.0).take(2 * n)), 1
+        ).unwrap();
 
     let mut n = NeuralNetwork::new()
         .add_layer(2)   // input layer with two units
         .add_layer(3)   // hidden layer with two units
         .add_layer(1);  // output layer
 
-    let y = Matrix::from_vec(labels.clone(), labels.len(), 1).unwrap();
-
     // gradient descent with 500 iterations
     for _ in (0..500) {
-        let mut d = n.derivatives(&x, &y);
+        let mut d = n.derivatives(&x, &labels);
         for j in &mut d { // alpha
             j.imul_scalar(-20.0);
         }
@@ -45,8 +43,8 @@ fn main() {
 
     // create the matrix for the contour plot
     let mut p = Matrix::<f64>::new();
-    for y in (-1.0).linspace(2.0, 90) {
-        for x in (-1.0).linspace(2.0, 90) {
+    for y in (-1.0).linspace(2.0, 100) {
+        for x in (-1.0).linspace(2.0, 100) {
             p.add_row(&[x, y, *n.predict(&[x, y]).first().unwrap()]);
         }
     }
@@ -54,12 +52,12 @@ fn main() {
     builder()
         // contour plot
         .add_matrix("P = $$", &p)
-        .add("tx = ty = linspace(-1, 1.96666666, 90)")
-        .add("contour(tx, ty, reshape(P(:,3), 90, 90))")
+        .add("tx = ty = linspace(-1, 2, 100)")
+        .add("contour(tx, ty, reshape(P(:, 3), 10, 10))")
         .add("hold on")
         // examples
         .add_matrix("X = $$", &x)
-        .add_vector("y = $$", &labels)
+        .add_vector_iter("y = $$", labels.values())
         .add("plot(X(y == 0, 1), X(y == 0, 2), 'd', 'markersize', 6, 'markerfacecolor', 'yellow', 'color', 'black')")
         .add("plot(X(y == 1, 1), X(y == 1, 2), 'o', 'markersize', 6, 'markerfacecolor', 'blue', 'color', 'black')")
         .add("axis([-1, 2, -1, 2])")
