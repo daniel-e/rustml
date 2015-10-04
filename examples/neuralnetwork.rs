ef1c1dfec19743270570f6b7adcdee4011c0b1ca
Ano Nym <detzold@gmail.com>
2015-10-04 19:05:36 +0200

    not working

--- examples/neuralnetwork.rs
+++ examples/neuralnetwork.rs
@@ -15,32 +15,34 @@ use rustml::vectors::Linspace;
 
 fn main() {
 
-    // create a random matrix where each element is either 0 or 1
+    // create the dataset
     let seed = [1, 2, 3, 4];
     let n = 50;
-    let examples = 
-        mixture_builder()
+    let x = mixture_builder()
             .add(n, normal_builder(seed).add(0.0, 0.2).add(0.0, 0.2))
             .add(n, normal_builder(seed).add(1.0, 0.2).add(1.0, 0.2))
             .add(n, normal_builder(seed).add(0.0, 0.2).add(1.0, 0.2))
             .add(n, normal_builder(seed).add(1.0, 0.2).add(0.0, 0.2))
-            .as_matrix();
+            .as_matrix()
+            .rm_column(0).unwrap();
 
+    // create the labels
     let labels = repeat(0.0).take(2 * n).chain(repeat(1.0).take(2 * n))
         .collect::<Vec<_>>();
 
     let mut n = NeuralNetwork::new()
         .add_layer(2)   // input layer with two units
-        .add_layer(3)   // hidden layer with two units
+        .add_layer(2)   // hidden layer with two units
         .add_layer(1);  // output layer
 
-    let x = examples.rm_column(0).unwrap();
     let y = Matrix::from_vec(labels.clone(), labels.len(), 1).unwrap();
 
-    for _ in (0..500) {
+    // gradient descent with 500 iterations
+    for _ in (0..2000) {
+        println!("{}", n.error(&x, &y));
         let mut d = n.derivatives(&x, &y);
         for j in &mut d { // alpha
-            j.imul_scalar(-20.0);
+            j.imul_scalar(-10.0);
         }
         n.update_weights(&d);
     }
@@ -60,10 +62,10 @@ fn main() {
         .add("contour(tx, ty, reshape(P(:,3), 90, 90))")
         .add("hold on")
         // examples
-        .add_matrix("X = $$", &examples)
+        .add_matrix("X = $$", &x)
         .add_vector("y = $$", &labels)
-        .add("plot(X(y == 0, 2), X(y == 0, 3), 'd', 'markersize', 6, 'markerfacecolor', 'yellow', 'color', 'black')")
-        .add("plot(X(y == 1, 2), X(y == 1, 3), 'o', 'markersize', 6, 'markerfacecolor', 'blue', 'color', 'black')")
+        .add("plot(X(y == 0, 1), X(y == 0, 2), 'd', 'markersize', 6, 'markerfacecolor', 'yellow', 'color', 'black')")
+        .add("plot(X(y == 1, 1), X(y == 1, 2), 'o', 'markersize', 6, 'markerfacecolor', 'blue', 'color', 'black')")
         .add("axis([-1, 2, -1, 2])")
         .add("grid on")
         .add("print -dpng -r100 '/tmp/nn.png'")
